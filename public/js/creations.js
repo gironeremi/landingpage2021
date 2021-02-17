def219d234013dd4f3814feea673da34f0a9504f
GIRONE <gironeremi@gmail.com>
2021-02-17 10:20:31 +0100

    Mise en ébauche d'un script pour automatiser l'affichage des réalisations... On verra plus tard!

---
+++ public/js/creations.js
@@ -0,0 +1,11 @@
+/*Début de boucle pour afficher les informations d'un fichier JSON. A reprendre plus tard.
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let creations = JSON.parse(this.responseText);
+    }
+};
+xmlhttp.open("GET", "myCreations.json", true);
+xmlhttp.send();
+
+console.log(creations);*/
